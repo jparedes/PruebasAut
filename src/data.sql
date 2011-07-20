ee6e6df0534f6329c9f86bdfe20de4ddeb075b7b
Juan Paredes <paredesucab@gmail.com>
2011-07-20 17:07:13 -0430

    Se agrega un insert a data.sql

--- src/data.sql
+++ src/data.sql
@@ -9,3 +9,6 @@ VALUES (null, 'dcarey', 'drumm3r', 'dcarey@toolband.com', '1991-12-01');
 
 INSERT INTO USUARIO (id, login, password, email, fecha_registro) 
 VALUES (null, 'jchancellor', 'b9ss1st', 'jchancellor@toolband.com', '1996-09-17');
+
+INSERT INTO USUARIO (id, login, password, email, fecha_registro) 
+VALUES (null, 'pdamour', 'b9ss1st', 'pdamour@lusk.com', '1996-12-01');
